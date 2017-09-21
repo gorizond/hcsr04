9377969299931f993f2125ec2d925b1b610af0dc
Negashev <i@negash.ru>
2017-09-21 16:09:51 +0000

    Update dockerfile aiohttp client

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,15 @@ FROM arm32v6/alpine
 
 WORKDIR /app
 
-RUN apk add --no-cache python3 \
-	&& apk add --no-cache --virtual .build-deps build-base python3-dev py3-pip \
-        && pip3 install hcsr04sensor aiohttp \
+RUN apk add --no-cache python3
+
+RUN pip3 install --no-cache-dir aiohttp
+
+RUN apk add --no-cache --virtual .build-deps build-base python3-dev py3-pip \
+        && pip3 install hcsr04sensor \
 	&& apk del .build-deps \
 	&& rm -rf /var/cache/apk/*
-CMD ["hcsr04.py"]
+
+ADD main.py main.py
+
+CMD ["python3", "main.py"]
---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+import aiohttp
+import asyncio
+from hcsr04sensor import sensor
+
+async def start_client(loop, url):
+    session = aiohttp.ClientSession()
+    was_connect = False
+    try:
+        ws = await session.ws_connect(url)
+        was_connect = True
+        print('Connected to server:')
+    except:
+        pass
+    else:
+        send_task = loop.create_task(send_to_server(ws))
+        async for msg in ws:
+            if msg.type == aiohttp.WSMsgType.TEXT:
+                pass
+            elif msg.type == aiohttp.WSMsgType.CLOSE:
+                break
+            elif msg.type == aiohttp.WSMsgType.CLOSED:
+                break
+            elif msg.type == aiohttp.WSMsgType.ERROR:
+                break
+        send_task.cancel()
+    finally:
+        session.close()
+        if was_connect:
+            print('Disconnected...')
+    return
+
+
+async def send_to_server(ws):
+    value = sensor.Measurement(int(os.getenv('TRIG_PIN', 23)), int(os.getenv('ECHO_PIN', 24)))
+    while True:
+        await asyncio.sleep(1)
+        ws.send_str(value.distance_metric(value.raw_distance()))
+
+
+async def main(loop):
+    url = os.getenv('CORE_URL', 'ws://core:80/hcsr04')
+
+    while True:
+        await asyncio.sleep(1)
+        print('Connecting to server...')
+        session_task = loop.create_task(start_client(loop, url))
+        await session_task
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
+    loop.close()
